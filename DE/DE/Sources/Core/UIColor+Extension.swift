410d582cbfa844dce146565a2d86c4fcff9bb7e9
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-12-17 18:07:25 +0900

    [Chore] Project Setting

---
+++ DE/DE/Sources/Core/UIColor+Extension.swift
@@ -0,0 +1,26 @@
+// Copyright © 2024 DRINKIG. All rights reserved
+
+import UIKit
+
+struct AppColor {
+    static let wineClassPurple = "#7E13B1"
+}
+
+extension UIColor {
+    class func appPurple() -> UIColor {
+        return UIColor(hex: AppColor.wineClassPurple)!
+    }
+    
+    func toHex() -> String? {
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var a: CGFloat = 0
+        
+        if self.getRed(&r, green: &g, blue: &b, alpha: &a) {
+            let rgb: Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+            return String(format: "#%06x", rgb)
+        }
+        return nil
+    }
+}
