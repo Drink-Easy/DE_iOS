42ca216439cb404fc5d5040da89cd0d6b0c6ce0b
JustinLee02 <164829006+JustinLee02@users.noreply.github.com>
2024-12-19 21:44:29 +0900

    #11 [Chore] 파일 이름 정리

---
+++ DE/DE/Sources/Features/TastingNote/Models/ImageStackView.swift
@@ -0,0 +1,8 @@
+// Copyright © 2024 DRINKIG. All rights reserved
+
+import Foundation
+
+struct ImageStackView {
+    let images = [("RedImage", "레드"), ("WhiteImage", "화이트"), ("SparklingImage", "스파클링"), ("RoseImage", "로제"), ("ETCImage", "기타")]
+
+}
