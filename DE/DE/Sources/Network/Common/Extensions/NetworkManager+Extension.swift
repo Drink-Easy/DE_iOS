ccb375adefa9b58ae73da9d039e094940d154977
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-12-23 19:19:38 +0900

    #5 home 네트워크 레이어 개발
    + Common 폴더 파일 정리
    + 레거시 코드 제거

--- DE/DE/Sources/Network/Common/Extensions/NetworkManager+Extension.swift
+++ DE/DE/Sources/Network/Common/Extensions/NetworkManager+Extension.swift
@@ -3,18 +3,6 @@
 import Moya
 import Foundation
 
-protocol NetworkManager {
-    associatedtype Endpoint: TargetType
-    
-    var provider: MoyaProvider<Endpoint> { get }
-    
-    func request<T: Decodable>(
-        target: Endpoint,
-        decodingType: T.Type,
-        completion: @escaping (Result<T, NetworkError>) -> Void
-    )
-}
-
 extension NetworkManager {
     func request<T: Decodable>(
         target: Endpoint,
---
+++ DE/DE/Sources/Network/Home/Service/HomeService.swift
@@ -0,0 +1,29 @@
+// Copyright © 2024 DRINKIG. All rights reserved
+
+import Foundation
+import Moya
+
+final class HomeService: NetworkManager {
+    typealias Endpoint = HomeEndpoint
+    
+    // Provider 설정
+    let provider: MoyaProvider<HomeEndpoint>
+    
+    init(provider: MoyaProvider<HomeEndpoint>? = nil) {
+        // 플러그인 추가
+        let plugins: [PluginType] = [
+            CookiePlugin(),
+            NetworkLoggerPlugin(configuration: .init(logOptions: .verbose)) // 로그 플러그인
+        ]
+        
+        // provider 초기화
+        self.provider = provider ?? MoyaProvider<HomeEndpoint>(plugins: plugins)
+    }
+    
+    //MARK: - API func
+    
+    // 위시리스트 조회
+    func fetchHomeInfo(completion: @escaping (Result<HomeResponseDTO, NetworkError>) -> Void) {
+        request(target: .getHomeInfo, decodingType: HomeResponseDTO.self, completion: completion)
+    }
+}
