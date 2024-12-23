f7047781a8c6bf88b34a9b33f504d4ce8d977b3c
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-12-23 19:51:58 +0900

    URL 작성

--- DE/DE/Sources/Core/Utilities/Constants.swift
+++ DE/DE/Sources/Core/Utilities/Constants.swift
@@ -13,9 +13,15 @@ public struct Constants {
     }
     
     public struct API {
-        public static let baseURL = "https://drinkeg.com"
-        public static let commentURL = "https://drinkeg.com/comments"
-        public static let recommentURL = "https://drinkeg.com/recomments"
+        static let baseURL = "https://drinkeg.com"
+        static let commentURL = "\(baseURL)/comments"
+        static let recommentURL = "\(baseURL)/recomments"
+        static let tastingNoteURL = "\(baseURL)/tasting-note"
+        static let wineClassURL = "\(baseURL)/wine-class"
+        static let wineLectureURL = "\(baseURL)/wine-lecture"
+        static let wineLectureCompleteURL = "\(baseURL)/wine-lecture-complete"
+        static let wineURL = "\(baseURL)/wine"
+        static let wishlistURL = "\(baseURL)/wine-wishlist"
     }
     
     public struct Design {
