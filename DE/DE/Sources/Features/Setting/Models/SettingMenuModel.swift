46ceae2363916992a87891e7476cdbf0e0948e5a
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2024-12-31 23:22:57 +0900

    #34 [Feat] 테이블뷰 화면 이동 연결

--- DE/DE/Sources/Features/Setting/Models/SettingMenuModel.swift
+++ DE/DE/Sources/Features/Setting/Models/SettingMenuModel.swift
@@ -6,3 +6,14 @@ struct SettingMenuModel {
     let name: String
     let viewControllerType: UIViewController.Type // 뷰컨트롤러의 타입 저장
 }
+
+struct SettingMenuItems {
+    public static let accountInfo = SettingMenuModel(name: "계정 정보", viewControllerType: AccountInfoViewController.self)
+    public static let wishList = SettingMenuModel(name: "위시리스트", viewControllerType: WishListViewController.self)
+    public static let ownedWine = SettingMenuModel(name: "내 보유 와인", viewControllerType: MyOwnedWineViewController.self)
+    public static let schedule = SettingMenuModel(name: "내 모임 일정", viewControllerType: MyPartyScheduleViewController.self)
+    public static let notice = SettingMenuModel(name: "공지사항", viewControllerType: NoticeViewController.self)
+    public static let appInfo = SettingMenuModel(name: "앱 정보", viewControllerType: AppInfoViewController.self)
+    public static let inquiry = SettingMenuModel(name: "1:1 문의하기", viewControllerType: InquiryViewController.self)
+}
+
--- DE/DE/Sources/Features/Setting/ViewControllers/SettingMenuViewController.swift
+++ DE/DE/Sources/Features/Setting/ViewControllers/SettingMenuViewController.swift
@@ -4,12 +4,50 @@ import UIKit
 import CoreModule
 import Network
 
-class SettingMenuViewController : UIViewController {
+public final class SettingMenuViewController : UIViewController {
+    private var tableView = UITableView()
+    
+    private let settingMenuItems: [SettingMenuModel] = [
+        SettingMenuItems.accountInfo,
+        SettingMenuItems.wishList,
+        SettingMenuItems.ownedWine,
+        SettingMenuItems.schedule,
+        SettingMenuItems.notice,
+        SettingMenuItems.appInfo,
+        SettingMenuItems.inquiry
+    ]
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
+    }
+
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
     }
 
 }
 
+extension SettingMenuViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settingMenuItems.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = settingMenuItems[indexPath.row].name
+        return cell
+    }
+}
+
+extension SettingMenuViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = settingMenuItems[indexPath.row]
+        let vc = selectedItem.viewControllerType.init() // 해당 뷰컨트롤러 생성
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
