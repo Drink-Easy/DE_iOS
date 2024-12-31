e9a3d07b12ceb43366e8fad04485d78c5a3ad87b
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-01-01 00:59:12 +0900

    #47 #36 [Feat] user data 세팅, [Fix] 탭바 루트뷰이동

---
+++ DE/DE/Sources/Core/DB/DataModels/UserData.swift
@@ -0,0 +1,13 @@
+// Copyright © 2024 DRINKIG. All rights reserved
+
+import Foundation
+import SwiftData
+
+@Model
+public class UserData {
+    var userName: String
+    
+    init(userName: String) {
+        self.userName = userName
+    }
+}
