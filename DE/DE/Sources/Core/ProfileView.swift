3e0dc4a080dc2e296075fa5b6f57e955a738d4ad
이예성 <yeseong_lee@naver.com>
2025-01-02 20:47:07 +0900

    #30 [feat] 취향 찾기 기능 폴더링

---
+++ DE/DE/Sources/Core/ProfileView.swift
@@ -0,0 +1,97 @@
+// Copyright © 2024 DRINKIG. All rights reserved
+
+import UIKit
+
+import SnapKit
+import Then
+
+public class ProfileView: UIView {
+    
+    public let profileImageView = UIImageView().then {
+        $0.image = UIImage(named: "profilePlaceholder")
+        $0.contentMode = .scaleAspectFill
+        $0.layer.cornerRadius = 50
+        $0.clipsToBounds = true
+    }
+    
+    public let profileImageEditButton = UIButton().then {
+        $0.backgroundColor = AppColor.purple100
+        $0.layer.cornerRadius = 15
+    }
+    
+    public let profileImageIconView = UIImageView().then {
+        let iconImage = UIImage(systemName: "camera.fill")?.withRenderingMode(.alwaysTemplate)
+        $0.image = iconImage
+        $0.tintColor = .white
+        $0.clipsToBounds = true
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    public let nicknameTextField = CustomTextFieldView(
+        descriptionLabelText: "닉네임",
+        textFieldPlaceholder: "닉네임을 입력해 주세요"
+    )
+    
+    public let myLocationTextField = CustomTextFieldView(
+        descriptionLabelText: "내 동네",
+        textFieldPlaceholder: ""
+    ).then {
+        $0.textField.isUserInteractionEnabled = false
+    }
+    
+    public let locationImageIconButton = UIButton().then {
+        let iconImage = UIImage(named: "MyLocation")?.withRenderingMode(.alwaysTemplate)
+        $0.setImage(iconImage, for: .normal)
+        $0.tintColor = AppColor.gray70
+        $0.imageView?.contentMode = .scaleAspectFit
+        $0.clipsToBounds = true
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+        setupConstraints()
+        
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        [profileImageView, profileImageEditButton, profileImageIconView, nicknameTextField, myLocationTextField, locationImageIconButton].forEach {
+            addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(48)
+            make.centerX.equalToSuperview()
+            make.width.height.equalTo(100)
+        }
+        profileImageEditButton.snp.makeConstraints { make in
+            make.width.height.equalTo(30)
+            make.trailing.equalTo(profileImageView.snp.trailing)
+            make.centerY.equalTo(profileImageView.snp.bottom).offset(-20)
+        }
+        profileImageIconView.snp.makeConstraints { make in
+            make.width.height.equalTo(20)
+            make.centerX.centerY.equalTo(profileImageEditButton)
+        }
+        nicknameTextField.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(32)
+            make.leading.trailing.equalToSuperview().inset(24)
+        }
+        myLocationTextField.snp.makeConstraints { make in
+            make.top.equalTo(nicknameTextField.snp.bottom).offset(20)
+            make.leading.equalToSuperview().inset(24)
+            make.trailing.equalToSuperview().inset(72)
+        }
+        locationImageIconButton.snp.makeConstraints { make in
+            make.width.height.equalTo(24)
+            make.leading.equalTo(myLocationTextField.snp.trailing).offset(16)
+            make.bottom.equalTo(myLocationTextField.snp.bottom).offset(-12)
+        }
+    }
+}
---
+++ DE/DE/Sources/Features/UserSurvey/ViewControllers/GetProfileVC.swift
@@ -0,0 +1,163 @@
+// Copyright © 2024 DRINKIG. All rights reserved
+
+import UIKit
+import PhotosUI
+
+import SnapKit
+import Then
+
+import CoreModule
+
+class GetProfileVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    private let navigationBarManager = NavigationBarManager()
+    
+    private let profileView = ProfileView()
+    
+    private let headerLabel = UILabel().then {
+        $0.text = "프로필을 만들어 보세요!"
+        $0.font = UIFont.ptdSemiBoldFont(ofSize: 24)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+    }
+    
+    let startButton = CustomButton(
+        title: "다음",
+        titleColor: .white,
+        isEnabled: false
+    ).then {
+        $0.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = AppColor.bgGray
+        
+        setupUI()
+        setupConstraints()
+        setupNavigationBar()
+        setupActions()
+        
+        configureTapGestureForDismissingPicker()
+    }
+        
+    // MARK: - 네비게이션 바 설정
+    func setupNavigationBar() {
+        navigationBarManager.addBackButton(
+            to: navigationItem,
+            target: self,
+            action: #selector(backButtonTapped),
+            tintColor: AppColor.gray70!
+        )
+    }
+    
+    // MARK: UI Component 설정
+    func setupUI() {
+        [headerLabel, profileView, startButton].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    func setupConstraints() {
+        headerLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(16)
+            make.leading.equalToSuperview().inset(30)
+        }
+        profileView.snp.makeConstraints { make in
+            make.top.equalTo(headerLabel.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(Constants.superViewHeight * 0.5)
+        }
+        startButton.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().offset(-40)
+            make.leading.trailing.equalToSuperview().inset(24)
+        }
+    }
+
+    func setupActions() {
+        profileView.profileImageEditButton.addTarget(self, action: #selector(selectProfileImage), for: .touchUpInside)
+        profileView.nicknameTextField.textField.addTarget(self, action: #selector(checkFormValidity), for: .editingDidEnd)
+        profileView.myLocationTextField.textField.addTarget(self, action: #selector(checkFormValidity), for: .editingDidEnd)
+        profileView.locationImageIconButton.addTarget(self, action: #selector(getMyLocation), for: .touchUpInside)
+    }
+    
+    //MARK: Functions
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func startButtonTapped() {
+        //TODO: 취향 찾기 뷰컨 연결
+    }
+    
+    func configureTapGestureForDismissingPicker() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissDatePicker))
+        tapGesture.cancelsTouchesInView = false
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc func dismissDatePicker() {
+        view.endEditing(true)
+    }
+    
+    // 프로필 이미지 선택
+    @objc func selectProfileImage() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images // 이미지만 선택 가능
+        configuration.selectionLimit = 1 // 하나의 이미지만 선택
+
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+    
+    @objc func getMyLocation() {
+        //TODO: 지역 연결 로직 작성
+    }
+    
+    // 폼 유효성 검사
+    @objc func checkFormValidity() {
+        let isNicknameValid = !(profileView.nicknameTextField.textField.text?.isEmpty ?? true)
+        let isLocationValid = !(profileView.myLocationTextField.textField.text?.isEmpty ?? true)
+        let isImageSelected = profileView.profileImageView.image != nil
+        print(isImageSelected)
+        let isFormValid = isNicknameValid && isLocationValid && isImageSelected
+
+        startButton.isEnabled = isFormValid
+        startButton.isEnabled(isEnabled: isFormValid)
+    }
+}
+
+extension GetProfileVC: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+
+        guard let result = results.first else { return }
+
+        if result.itemProvider.canLoadObject(ofClass: UIImage.self) {
+            result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] (object, error) in
+                guard let self = self else { return }
+                if let image = object as? UIImage {
+                    DispatchQueue.main.async {
+                        self.profileView.profileImageView.image = image
+                        //TODO: 이미지 데이터 어디에 저장?
+                    }
+                } else {
+                    print("이미지를 불러오지 못했습니다: \(error?.localizedDescription ?? "알 수 없는 오류")")
+                }
+            }
+        } else {
+            print("선택한 항목에서 이미지를 로드할 수 없습니다.")
+        }
+    }
+}
