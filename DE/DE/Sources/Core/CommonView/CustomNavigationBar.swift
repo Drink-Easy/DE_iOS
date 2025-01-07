9f0979eeb373d11f8851054e003dc09778752556
Hyeon-Ju <lhjivy1024@gmail.com>
2025-01-07 11:37:45 +0900

    #59 Feat: 위시리스트 뷰 구현

--- DE/DE/Sources/Core/CommonView/CustomNavigationBar.swift
+++ DE/DE/Sources/Core/CommonView/CustomNavigationBar.swift
@@ -29,7 +29,7 @@ public class NavigationBarManager {
     }
     
     // MARK: - 네비게이션 타이틀 설정
-    public func setTitle(to navigationItem: UINavigationItem, title: String, textColor: UIColor = .label, font: UIFont = .systemFont(ofSize: 18, weight: .bold)) {
+    public func setTitle(to navigationItem: UINavigationItem, title: String, textColor: UIColor = .label, font: UIFont = .ptdSemiBoldFont(ofSize: 18)) {
         let titleLabel = UILabel()
         titleLabel.text = title
         titleLabel.font = font
@@ -42,7 +42,7 @@ public class NavigationBarManager {
         to navigationItem: UINavigationItem,
         title: String,
         textColor: UIColor = .label,
-        font: UIFont = .systemFont(ofSize: 18, weight: .bold)
+        font: UIFont = .ptdSemiBoldFont(ofSize: 18)
     ) -> UILabel {
         let titleLabel = UILabel()
         titleLabel.text = title
@@ -52,28 +52,28 @@ public class NavigationBarManager {
         navigationItem.titleView = titleLabel
         return titleLabel  // UILabel(smallTitle) 반환
     }
-  
+    
     public func addLeftRightButtons(
-            to navigationItem: UINavigationItem,
-            leftIcon: String,
-            leftAction: Selector,
-            rightIcon: String,
-            rightAction: Selector,
-            target: Any?,
-            tintColor: UIColor = .label
-        ) {
-            // 왼쪽 버튼 생성
-            let leftButton = UIButton(type: .system)
-            leftButton.setImage(UIImage(systemName: leftIcon), for: .normal)
-            leftButton.tintColor = tintColor
-            leftButton.addTarget(target, action: leftAction, for: .touchUpInside)
-            navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
-
-            // 오른쪽 버튼 생성
-            let rightButton = UIButton(type: .system)
-            rightButton.setImage(UIImage(systemName: rightIcon), for: .normal)
-            rightButton.tintColor = tintColor
-            rightButton.addTarget(target, action: rightAction, for: .touchUpInside)
-            navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightButton)
-        }
+        to navigationItem: UINavigationItem,
+        leftIcon: String,
+        leftAction: Selector,
+        rightIcon: String,
+        rightAction: Selector,
+        target: Any?,
+        tintColor: UIColor = .label
+    ) {
+        // 왼쪽 버튼 생성
+        let leftButton = UIButton(type: .system)
+        leftButton.setImage(UIImage(systemName: leftIcon), for: .normal)
+        leftButton.tintColor = tintColor
+        leftButton.addTarget(target, action: leftAction, for: .touchUpInside)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: leftButton)
+        
+        // 오른쪽 버튼 생성
+        let rightButton = UIButton(type: .system)
+        rightButton.setImage(UIImage(systemName: rightIcon), for: .normal)
+        rightButton.tintColor = tintColor
+        rightButton.addTarget(target, action: rightAction, for: .touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: rightButton)
+    }
 }
--- DE/DE/Sources/Features/Setting/ViewControllers/WishListViewController.swift
+++ DE/DE/Sources/Features/Setting/ViewControllers/WishListViewController.swift
@@ -1,25 +1,70 @@
 // Copyright © 2024 DRINKIG. All rights reserved
 
 import UIKit
+import SnapKit
+import Then
+import CoreModule
 
 class WishListViewController: UIViewController {
+    
+    private let navigationBarManager = NavigationBarManager()
+    private let networkService = WishlistService()
+    
+    private lazy var searchResultTableView = UITableView().then {
+        $0.register(SearchResultTableViewCell.self, forCellReuseIdentifier: "SearchResultTableViewCell")
+        $0.separatorInset = UIEdgeInsets(top: 0, left: 6, bottom: 0, right: 6)
+        $0.backgroundColor = Constants.AppColor.grayBG
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .black
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = AppColor.bgGray
+        setupNavigationBar()
+        addComponents()
+        setConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
-    */
+    
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    func setupNavigationBar() {
+        navigationBarManager.addBackButton(
+            to: navigationItem,
+            target: self,
+            action: #selector(backButtonTapped),
+            tintColor: AppColor.gray70!
+        )
+        
+        navigationBarManager.setTitle(
+            to: navigationItem,
+            title: "위시리스트",
+            textColor: AppColor.black ?? .black
+        )
+    }
+    
+    @objc private func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func addComponents() {
+        [searchResultTableView].forEach { view.addSubview($0) }
+    }
+
+    private func setConstraints() {
+        searchResultTableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(11)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(18)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    
 
 }
