852e5166e7e194ec5bbd61edfaf8fcc4f6c9e10c
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-01-08 00:15:04 +0900

    #5 [Feat] 마이페이지/취향찾기 API

--- DE/DE/Sources/Network/Member/Service/MemberService.swift
+++ DE/DE/Sources/Network/Member/Service/MemberService.swift
@@ -21,9 +21,38 @@ public final class MemberService : NetworkManager {
     }
     
     // MARK: - DTO funcs
+    /// 취향찾기 데이터 생성 함수
+    public func makeMemberInfoRequestDTO(name: String, isNewbie: Bool, monthPrice: Int, wineSort: [String], wineArea: [String], wineVariety: [String], region: String) -> MemberRequestDTO {
+        return MemberRequestDTO(name: name, isNewbie: isNewbie, monthPrice: monthPrice, wineSort: wineSort, wineArea: wineArea, wineVariety: wineVariety, region: region)
+    }
     
+    /// 프로필 업데이트 데이터 생성 함수
+    public func makeMemberInfoUpdateRequestDTO(username: String?, city: String?) -> MemberUpdateRequest {
+        return MemberUpdateRequest(username: username, city: city)
+    }
+    
+    /// 닉네임 체크 API
     public func checkNickname(name: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
         request(target: .checkNickname(nickname: name), decodingType: String.self, completion: completion)
     }
     
+    /// 취향찾기 등록 API
+    public func patchUserSurvey(imageName fileName: String, imageData fileData: Data, body: MemberRequestDTO, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        request(target: .patchMemberInfo(fileData: fileData, fileName: fileName, body: body), decodingType: String.self, completion: completion)
+    }
+    
+    /// 개인정보 갱신 API
+    public func patchUserInfo(imageName fileName: String, imageData fileData: Data, body: MemberUpdateRequest, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        request(target: .patchMemeberPersonalInfo(fileData: fileData, fileName: fileName, body: body), decodingType: String.self, completion: completion)
+    }
+    
+    /// 개인정보 불러오기 API
+    public func fetchUserInfo(completion: @escaping (Result<MemberInfoResponse, NetworkError>) -> Void) {
+        request(target: .getMemberInfo, decodingType: MemberInfoResponse.self, completion: completion)
+    }
+    
+    /// 사용자 탈퇴 API
+    public func deleteUser(completion: @escaping (Result<String, NetworkError>) -> Void) {
+        request(target: .deleteMember, decodingType: String.self, completion: completion)
+    }
 }
