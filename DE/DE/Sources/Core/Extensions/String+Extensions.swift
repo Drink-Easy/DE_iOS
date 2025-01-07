8a974840c0cd984a12f3e28e9bfeb1ad256b8560
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-01-07 10:46:05 +0900

    #4 [Chore] Core Foldering

--- DE/DE/Sources/Core/Extensions/String+Extensions.swift
+++ DE/DE/Sources/Core/Extensions/String+Extensions.swift
@@ -50,4 +50,16 @@ extension String {
         // 높이 반환 (lineSpacing 반영)
         return ceil(boundingBox.height)
     }
+    
+    /// 특정 텍스트의 색상을 변경한 `NSAttributedString` 반환
+    public func withColor(for text: String, color: UIColor) -> NSAttributedString {
+        let attributedString = NSMutableAttributedString(string: self)
+        
+        if let range = self.range(of: text) {
+            let nsRange = NSRange(range, in: self)
+            attributedString.addAttribute(.foregroundColor, value: color, range: nsRange)
+        }
+        
+        return attributedString
+    }
 }
