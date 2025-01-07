0ec32f78b9fe8c7c78be9a487f9057c067671092
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-01-07 17:39:39 +0900

    #47 [Feat] api 콜수 카운터

--- DE/DE/Sources/Core/DB/DataModels/APICallCountModel.swift
+++ DE/DE/Sources/Core/DB/DataModels/APICallCountModel.swift
@@ -1,2 +1,45 @@
 // Copyright © 2024 DRINKIG. All rights reserved
 
+import Foundation
+import SwiftData
+
+@Model
+public class APICounter {
+    public var postCount: Int = 0
+    public var deleteCount: Int = 0
+    public var patchCount: Int = 0
+
+    init(postCount: Int = 0, deleteCount: Int = 0, patchCount: Int = 0) {
+        self.postCount = postCount
+        self.deleteCount = deleteCount
+        self.patchCount = patchCount
+    }
+
+    /// POST 호출 카운트 증가
+    func incrementPost() {
+        postCount += 1
+    }
+
+    /// DELETE 호출 카운트 증가
+    func incrementDelete() {
+        deleteCount += 1
+    }
+
+    /// PATCH 호출 카운트 증가
+    func incrementPatch() {
+        patchCount += 1
+    }
+}
+
+@Model
+public class APIControllerCounter {
+    var name: String // 컨트롤러 이름
+    @Relationship var counter: APICounter // APICounter와 연관 관계 설정
+    @Relationship public var user: UserData? // 부모 관계 추가
+
+    init(name: String, counter: APICounter, user: UserData? = nil) {
+        self.name = name
+        self.counter = counter
+        self.user = user
+    }
+}
--- DE/DE/Sources/Core/DB/DataModels/UserData.swift
+++ DE/DE/Sources/Core/DB/DataModels/UserData.swift
@@ -7,6 +7,18 @@ import SwiftData
 public class UserData {
     @Attribute(.unique) public var userId : Int
     public var userName: String?
-    @Relationship public var wines : [WineList] = []
-        var wishlist: Wishlist
+    
+    @Relationship public var wines : [WineList] = [] // 홈에서 사용하는 와인
+    @Relationship public var controllerCounters: [APIControllerCounter] = [] // API 호출 횟수 기록
+    @Relationship public var wishlist: Wishlist? // 위시리스트 와인 저장
+    // 테이스팅 노트 저장
+    // 보유와인 저장
+    
+    init(userId: Int, userName: String? = nil, wines: [WineList], controllerCounters: [APIControllerCounter], wishlist: Wishlist? = nil) {
+        self.userId = userId
+        self.userName = userName
+        self.wines = wines
+        self.controllerCounters = controllerCounters
+        self.wishlist = wishlist
+    }
 }
