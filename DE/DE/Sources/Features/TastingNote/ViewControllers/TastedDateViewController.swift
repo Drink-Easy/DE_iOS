7a5c8f188f4d5f80a86669188758ca5abffc5276
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-01-21 02:30:34 +0900

    #76 [Feat] 테노 날짜 선택 테스트 완료

--- DE/DE/Sources/Features/TastingNote/ViewControllers/TastedDateViewController.swift
+++ DE/DE/Sources/Features/TastingNote/ViewControllers/TastedDateViewController.swift
@@ -4,19 +4,24 @@ import UIKit
 import CoreModule
 import Network
 
+// 와인 시음 날짜 선택 1번
+
 public class TastedDateViewController: UIViewController {
     
     var registerWine: MyOwnedWine = MyOwnedWine()
 
-    let tastedDateView = TastedDateView()
+    lazy var tastedDateView = TastedDateView()
+    let tnManger = NewTastingNoteManager.shared
+    let wineData = TNWineDataManager.shared
     var selectedDate: DateComponents?
     let navigationBarManager = NavigationBarManager()
 
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        DispatchQueue.main.async {
-            self.tastedDateView.updateUI(wineName: self.registerWine.wineName)
-        }
+        tastedDateView.topView.title.text = "만약에 와인 이름이 졸라 길다면 2025 02 12 띄어쓰기는 잘함"
+//        tastedDateView.topView.title.text = self.wineData.wineName
+//        self.tastedDateView.updateUI(wineName: self.registerWine.wineName)
+//        self.tastedDateView.updateUI(wineName: self.wineData.wineName)
     }
     
     public override func viewDidLoad() {
@@ -27,10 +32,13 @@ public class TastedDateViewController: UIViewController {
     }
     
     func setupUI() {
-        view.backgroundColor = .white
+        view.backgroundColor = AppColor.bgGray
+        
         view.addSubview(tastedDateView)
         tastedDateView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(10)
+            make.leading.trailing.equalToSuperview().inset(24)
+            make.bottom.equalToSuperview()
         }
     }
     
@@ -46,7 +54,7 @@ public class TastedDateViewController: UIViewController {
             to: navigationItem,
             target: self,
             action: #selector(prevVC),
-            tintColor: AppColor.gray80!
+            tintColor: AppColor.gray90!
         )
     }
     
@@ -61,14 +69,11 @@ public class TastedDateViewController: UIViewController {
         }
         
         if let date = Calendar.current.date(from: selectedDate) {
-            // 날짜를 `String`으로 변환
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yyyy-MM-dd"
             let dateString = dateFormatter.string(from: date)
             
-            // UserDefaults에 저장
-            UserDefaults.standard.set(dateString, forKey: "tasteDate")
-            print("날짜 저장됨: \(dateString)")
+            tnManger.saveTasteDate(dateString)
         }
         
         let nextVC = ChooseWineColorViewController()
@@ -82,22 +87,21 @@ extension TastedDateViewController: UICalendarSelectionSingleDateDelegate {
         guard let validDateComponents = dateComponents else {
             return
         }
-        // 선택된 날짜를 저장
         selectedDate = validDateComponents
-        
-        // 선택된 날짜에 대한 장식 업데이트
         tastedDateView.calender.reloadDecorations(forDateComponents: [validDateComponents], animated: true)
+        self.tastedDateView.nextButton.isEnabled(isEnabled: true)
     }
+    
+    
 }
 
 extension TastedDateViewController: UICalendarViewDelegate {
     public func calendarView(_ calendarView: UICalendarView, decorationFor dateComponents: DateComponents) -> UICalendarView.Decoration? {
-        // Check if the current date matches the selected date
         if dateComponents == selectedDate {
             return .customView {
                 let backgroundView = UIView()
                 backgroundView.backgroundColor = AppColor.purple100
-                backgroundView.layer.cornerRadius = 18 // 원형으로 만들기 위해 cornerRadius를 반지름으로 설정
+                backgroundView.layer.cornerRadius = 18
                 backgroundView.layer.masksToBounds = true
                 
                 return backgroundView
