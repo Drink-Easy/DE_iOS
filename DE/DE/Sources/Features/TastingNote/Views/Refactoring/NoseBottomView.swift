05efb2a56abe04edbbf70982334e1355a8c9228a
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-01-23 22:44:07 +0900

    #76 [Fix] 레거시 코드 일단 정상 작동하게 수정

---
+++ DE/DE/Sources/Features/TastingNote/Views/Refactoring/NoseBottomView.swift
@@ -0,0 +1,82 @@
+// Copyright © 2024 DRINKIG. All rights reserved
+
+import UIKit
+import CoreModule
+import SnapKit
+
+// 계열 선택 뷰
+class NoseBottomView: UIView {
+    
+    let scrollView = UIScrollView().then {
+        $0.backgroundColor = .clear
+        $0.showsVerticalScrollIndicator = false
+    }
+    
+    lazy var contentView = UIView().then {
+        $0.backgroundColor = .clear
+    }
+    
+    var layout = NewLeftAlignedCollectionViewFlowLayout().then {
+        $0.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        $0.minimumInteritemSpacing = 10
+        $0.minimumLineSpacing = 12
+    }
+    
+    public lazy var noseCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout).then {
+        $0.setCollectionViewLayout(layout, animated: true)
+        $0.backgroundColor = .clear
+        $0.tag = 0
+    }
+    
+    lazy var nextButton = CustomButton(title: "다음", isEnabled: false)
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addComponents()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func addComponents() {
+        contentView.addSubview(noseCollectionView)
+        contentView.addSubview(nextButton)
+        scrollView.addSubview(contentView)
+        addSubview(scrollView)
+    }
+    
+    private func setConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(scrollView.snp.width)
+            make.bottom.equalTo(nextButton.snp.bottom).offset(40) // 콘텐츠 높이 설정
+        }
+        
+        noseCollectionView.snp.makeConstraints { make in
+            make.leading.trailing.top.equalToSuperview()
+            make.height.equalTo(320) // 초기 높이
+        }
+        
+        nextButton.snp.makeConstraints { make in
+            make.top.equalTo(noseCollectionView.snp.bottom).offset(60)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(60)
+        }
+    }
+    
+    func updateNoseCollectionViewHeight() {
+        noseCollectionView.layoutIfNeeded() // 레이아웃 업데이트
+        let contentHeight = noseCollectionView.contentSize.height
+        noseCollectionView.snp.updateConstraints { make in
+            make.height.equalTo(contentHeight)
+        }
+    }
+    
+}
--- DE/DE/Sources/Features/TastingNote/Views/Refactoring/NoseTopView.swift
+++ DE/DE/Sources/Features/TastingNote/Views/Refactoring/NoseTopView.swift
@@ -21,24 +21,22 @@ class NoseTopView: UIView {
         $0.font = .ptdSemiBoldFont(ofSize: 18)
         $0.textColor = AppColor.purple100
     }
-    public lazy var selectedCollectionView = UICollectionView()
     
-//    let selectedCollectionView: UICollectionView = {
-//        let layout = LeftAlignedCollectionViewFlowLayout()
-//        layout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
-//        layout.minimumInteritemSpacing = 10
-//        layout.minimumLineSpacing = 10
-//        layout.sectionInset = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-//        layout.headerReferenceSize = CGSize(width: Constants.superViewWidth - 48, height: 50) // 헤더 가로사이즈 superview에 맞게하고싶음
-//        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
-//        cv.backgroundColor = .clear
-//        cv.tag = 1
-//        return cv
-//    }()
+    var layout = NewLeftAlignedCollectionViewFlowLayout().then {
+        $0.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        $0.minimumInteritemSpacing = 10
+        $0.minimumLineSpacing = 12
+    }
+    
+    public lazy var selectedCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout).then {
+        $0.backgroundColor = .clear
+        $0.tag = 1
+    }
     
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
         addComponents()
         setConstraints()
     }
@@ -60,6 +58,7 @@ class NoseTopView: UIView {
         propertyHeader.snp.makeConstraints { make in
             make.top.equalTo(header.snp.bottom).offset(40) // TODO : 동적 기기 대응
             make.leading.trailing.equalToSuperview()
+            make.height.greaterThanOrEqualTo(30)
         }
         
         noseDescription.snp.makeConstraints { make in
@@ -79,4 +78,12 @@ class NoseTopView: UIView {
         }
     }
     
+    
+//    func updateSelectedCollectionViewHeight() {
+//        selectedCollectionView.layoutIfNeeded() // 레이아웃 업데이트
+//        let contentHeight = selectedCollectionView.contentSize.height
+//        selectedCollectionView.snp.updateConstraints { make in
+//            make.height.equalTo(contentHeight)
+//        }
+//    }
 }
