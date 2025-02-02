37b665602f9f7d637a248b591b11768a62382e8a
이예성 <yeseong_lee@naver.com>
2025-02-03 02:51:29 +0900

    #89 [feat] 인디케이터 추가 설정

--- DE/DE/Sources/Core/Extensions/UIView+Extensions.swift
+++ DE/DE/Sources/Core/Extensions/UIView+Extensions.swift
@@ -4,8 +4,18 @@ import UIKit
 
 extension UIView {
     public func showBlockingView() {
+        //인디케이터 시작, 투명 뷰
         let blockingView = UIView(frame: self.bounds)
         blockingView.backgroundColor = .clear
+        blockingView.tag = 999  // 태그를 지정하여 쉽게 제거 가능
+        self.addSubview(blockingView)
+        indicator.startAnimating()
+    }
+    
+    public func showColorBlockingView() {
+        //인디케이터 시작, 배경색 뷰로 뷰컨 데이터 안 보이게
+        let blockingView = UIView(frame: self.bounds)
+        blockingView.backgroundColor = AppColor.bgGray
         blockingView.tag = 999  // 태그를 지정하여 쉽게 제거 가능
         self.addSubview(blockingView)
         indicator.startAnimating()
