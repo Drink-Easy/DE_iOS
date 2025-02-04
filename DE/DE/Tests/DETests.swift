e0bbe32506d6d09074e29dc63ec8d5caa2ce9388
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-02-05 00:25:51 +0900

    #89 [fix] 테스트코드 시도

--- DE/DE/Tests/DETests.swift
+++ DE/DE/Tests/DETests.swift
@@ -1,8 +1,33 @@
 import Foundation
 import XCTest
+import Network
+
+@testable import DRINKIG
 
 final class DETests: XCTestCase {
-    func test_twoPlusTwo_isFour() {
-        XCTAssertEqual(2+2, 4)
+    
+    let authService = AuthService()
+    
+    func testLogin() async {
+        let loginMockData = authService.makeLoginDTO(username: "Test@g.com", password: "test1234@")
+        
+        do {
+            let response = try await authService.login(data: loginMockData)
+            XCTAssertNotNil(response, "로그인 응답이 nil입니다!") // ✅ 응답 확인
+            print("✅ 로그인 성공: \(response)")
+        } catch {
+            XCTFail("로그인 실패: \(error.localizedDescription)")
+        }
     }
+    
+    func testLogout() async {
+        do {
+            let response = try await authService.logout()
+            XCTAssertNotNil(response, "로그아웃 응답이 nil입니다!") // ✅ 응답 확인
+            print("✅ 로그아웃 성공")
+        } catch {
+            XCTFail("로그아웃 실패: \(error.localizedDescription)")
+        }
+    }
+    
 }
