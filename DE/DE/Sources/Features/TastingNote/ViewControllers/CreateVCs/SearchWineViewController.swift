a31242f2026524cb2cd1262411f6dc191291187b
이예성 <yeseong_lee@naver.com>
2025-02-05 03:45:40 +0900

    [fix] 카카오 로그인 기기 테스트

--- DE/DE/Sources/Features/TastingNote/ViewControllers/CreateVCs/SearchWineViewController.swift
+++ DE/DE/Sources/Features/TastingNote/ViewControllers/CreateVCs/SearchWineViewController.swift
@@ -72,7 +72,7 @@ public class SearchWineViewController : UIViewController, UITableViewDelegate, U
     }
 
     private func showCharacterLimitAlert() {
-        let alert = UIAlertController(title: "경고", message: "최소 2자 이상 입력해 주세요.", preferredStyle: .alert)
+        let alert = UIAlertController(title: "", message: "검색어를 2자 이상 입력해 주세요.", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
