adaf22a4378893a78f0e4608e32489acc188e557
Hyeon-Ju <lhjivy1024@gmail.com>
2025-02-07 17:57:49 +0900

    #30 Feat: 패키지 토스터 -> 커스텀 토스터 적용

--- DE/DE/Sources/Features/UserSurvey/ViewControllers/Newbie/NewbieFoodViewController.swift
+++ DE/DE/Sources/Features/UserSurvey/ViewControllers/Newbie/NewbieFoodViewController.swift
@@ -91,7 +91,7 @@ extension NewbieFoodViewController: UICollectionViewDelegateFlowLayout, UICollec
             selectedItems.removeAll { $0 == selectedItem }
         } else {
             if selectedItems.count >= maxSelectionCount { // 이미 2개 선택
-                Toaster.shared.makeToast("선택할 수 있는 갯수를 초과했습니다.", .short)
+                showToastMessage(message: "선택할 수 있는 갯수를 초과했습니다.", yPosition: view.frame.height * 0.75)
                 return
             }
             // 새 아이템 선택
