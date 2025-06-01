4e24e24e17de34b6126d533f78a4e016aa55c697
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-06-01 20:38:06 +0900

    #162/Design 빈티지 연도 셀 UI 구현

---
+++ DE/DE/Sources/Core/CommonUI/Cells/VintageTableViewCell.swift
@@ -0,0 +1,70 @@
+// Copyright © 2025 DRINKIG. All rights reserved
+
+import UIKit
+
+import SnapKit
+import Then
+import DesignSystem
+
+public final class VintageTableViewCell: UITableViewCell {
+    // MARK: - Components
+    private var year = UILabel().then {
+        $0.textColor = AppColor.black
+        $0.font = UIFont.pretendard(.medium, size: 14)
+        $0.numberOfLines = 1
+        $0.lineBreakMode = .byTruncatingTail
+        $0.setLineSpacingPercentage(0.5)
+    }
+    
+    private var score = UILabel().then {
+        $0.textColor = AppColor.purple100
+        $0.font = UIFont.pretendard(.regular, size: 14)
+        $0.setLineSpacingPercentage(0.2)
+    }
+    
+    // MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupView()
+        addComponents()
+        constraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func prepareForReuse() {
+        super.prepareForReuse()
+        self.year.text = nil
+        self.score.text = nil
+    }
+    
+    // MARK: - SetUp
+    private func setupView() {
+        contentView.backgroundColor = AppColor.background
+    }
+    
+    private func addComponents() {
+        addSubviews(year, score)
+    }
+    
+    private func constraints() {
+        year.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(16)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        
+        score.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(20)
+            $0.centerY.equalTo(year)
+        }
+    }
+    
+    // MARK: - Configure
+    public func configure(year: Int, score: Double) {
+        self.year.text = "\(year)"
+        self.score.text = "★ \(String(format: "%.1f", score))"
+    }
+    
+}
