db61cef59ac3d07197f7a630f0ec197cc85db5be
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-05 15:50:33 +0900

    #166/feat: 보유와인DTO에 빈티지 필드 추가

--- DE/DE/Sources/Network/MyWine/Response/MyWineResponse.swift
+++ DE/DE/Sources/Network/MyWine/Response/MyWineResponse.swift
@@ -6,6 +6,7 @@ public struct MyWineResponse : Decodable {
     public let myWineId : Int
     public let wineId : Int
     public let wineName : String
+    public let vintageYear : Int
     public let wineSort : String
     public let wineCountry : String
     public let wineRegion : String
@@ -14,4 +15,32 @@ public struct MyWineResponse : Decodable {
     public let purchaseDate : String
     public let purchasePrice : Int
     public let period : Int
+    
+    public init(
+        myWineId: Int,
+        wineId: Int,
+        wineName: String,
+        vintageYear: Int,
+        wineSort: String,
+        wineCountry: String,
+        wineRegion: String,
+        wineVariety: String,
+        wineImageUrl: String,
+        purchaseDate: String,
+        purchasePrice: Int,
+        period: Int
+    ) {
+        self.myWineId = myWineId
+        self.wineId = wineId
+        self.wineName = wineName
+        self.vintageYear = vintageYear
+        self.wineSort = wineSort
+        self.wineCountry = wineCountry
+        self.wineRegion = wineRegion
+        self.wineVariety = wineVariety
+        self.wineImageUrl = wineImageUrl
+        self.purchaseDate = purchaseDate
+        self.purchasePrice = purchasePrice
+        self.period = period
+    }
 }
