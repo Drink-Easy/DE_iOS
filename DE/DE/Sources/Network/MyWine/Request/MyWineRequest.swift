8275d419a90183ea27268d38f2b733229a736be9
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-07-05 15:50:33 +0900

    #166/feat: 보유와인DTO에 빈티지 필드 추가

--- DE/DE/Sources/Network/MyWine/Request/MyWineRequest.swift
+++ DE/DE/Sources/Network/MyWine/Request/MyWineRequest.swift
@@ -4,21 +4,25 @@ import Foundation
 
 public struct MyWineRequest: Codable {
     public var wineId: Int
+    public var vintageYear: Int?
     public var purchaseDate : String
-    public var purchasePrice : Int
+    public var purchasePrice : Int?
     
-    init(wineId: Int, purchaseDate: String, purchasePrice: Int) {
+    init(wineId: Int, vintageYear: Int? = nil, purchaseDate: String, purchasePrice: Int? = nil) {
         self.wineId = wineId
+        self.vintageYear = vintageYear
         self.purchaseDate = purchaseDate
         self.purchasePrice = purchasePrice
     }
 }
 
 public struct MyWineUpdateRequest: Codable {
+    public var vintageYear: Int?
     public var purchaseDate : String?
     public var purchasePrice : Int?
     
-    init(purchaseDate: String? = nil, purchasePrice: Int? = nil) {
+    init(vintageYear: Int? = nil, purchaseDate: String? = nil, purchasePrice: Int? = nil) {
+        self.vintageYear = vintageYear
         self.purchaseDate = purchaseDate
         self.purchasePrice = purchasePrice
     }
