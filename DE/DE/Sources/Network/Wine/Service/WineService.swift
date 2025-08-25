2e8aec1529fefed9889fd2db0a1fa124d488a9fc
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-08-26 00:19:10 +0900

    #166/feat: 와인 검색/홈 와인 추천/와인 리뷰 조회 API에 빈티지 변경사항 적용

--- DE/DE/Sources/Network/Wine/Service/WineService.swift
+++ DE/DE/Sources/Network/Wine/Service/WineService.swift
@@ -13,7 +13,7 @@ public final class WineService: NetworkManager {
         // 플러그인 추가
         let plugins: [PluginType] = [
             CookiePlugin(),
-//            NetworkLoggerPlugin(configuration: .init(logOptions: .verbose)) // 로그 플러그인
+            NetworkLoggerPlugin(configuration: .init(logOptions: .verbose)) // 로그 플러그인
         ]
         
         // provider 초기화
@@ -24,27 +24,45 @@ public final class WineService: NetworkManager {
 
     /// 와인 검색
     public func fetchWines(searchName: String, page: Int) async throws -> PageSearchWineResponseDTO? {
-        return try await requestOptionalAsync(target: .getWines(searchName: searchName, page: page), decodingType: PageSearchWineResponseDTO.self)
+        return try await requestOptionalAsync(
+            target: .getWines(
+                searchName: searchName,
+                page: page
+            ),
+            decodingType: PageSearchWineResponseDTO.self
+        )
     }
 
     /// 선택 와인 정보 조회
-    public func fetchWineInfo(wineId: Int) async throws -> WineResponseWithThreeReviewsDTO? {
-        return try await requestOptionalAsync(target: .getWineInfo(wineId: wineId), decodingType: WineResponseWithThreeReviewsDTO.self)
+    public func fetchWineInfo(wineId: Int, vintageYear: Int?) async throws -> WineResponseWithThreeReviewsDTO? {
+        return try await requestOptionalAsync(
+            target: .getWineInfo(wineId: wineId, vintageYear: vintageYear),
+            decodingType: WineResponseWithThreeReviewsDTO.self
+        )
     }
     
     /// 선택 와인 리뷰 조회
-    public func fetchWineReviews(wineId: Int, sortType: String, page: Int) async throws -> PageResponseWineReviewResponse? {
-        return try await requestAsync(target: .getWineReview(wineId: wineId, sortType: sortType, page: page), decodingType: PageResponseWineReviewResponse.self)
+    public func fetchWineReviews(wineId: Int, vintageYear: Int?, sortType: String, page: Int) async throws -> PageResponseWineReviewResponse? {
+        return try await requestAsync(
+            target: .getWineReview(wineId: wineId, vintageYear: vintageYear, sortType: sortType, page: page),
+            decodingType: PageResponseWineReviewResponse.self
+        )
     }
     
     /// 추천 와인 조회
     public func fetchRecommendWines() async throws -> ([HomeWineDTO], TimeInterval?) {
-        return try await requestWithTimeAsync(target: .getRecommendWines, decodingType: [HomeWineDTO].self)
+        return try await requestWithTimeAsync(
+            target: .getRecommendWines,
+            decodingType: [HomeWineDTO].self
+        )
     }
     
     /// 인기 와인 조회
     public func fetchPopularWines() async throws -> ([HomeWineDTO], TimeInterval?) {
-        return try await requestWithTimeAsync(target: .getMostLikedWines, decodingType: [HomeWineDTO].self)
+        return try await requestWithTimeAsync(
+            target: .getMostLikedWines,
+            decodingType: [HomeWineDTO].self
+        )
     }
     
 }
