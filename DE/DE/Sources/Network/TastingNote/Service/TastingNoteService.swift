daceb3310c20a45f6e124f1602ba45a056e5212b
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-08-26 00:52:04 +0900

    #166/fix: tastingNote API 수정

--- DE/DE/Sources/Network/TastingNote/Service/TastingNoteService.swift
+++ DE/DE/Sources/Network/TastingNote/Service/TastingNoteService.swift
@@ -12,7 +12,7 @@ public final class TastingNoteService: NetworkManager {
         // 플러그인 추가
         let plugins: [PluginType] = [
             CookiePlugin(),
-            //            NetworkLoggerPlugin(configuration: .init(logOptions: .verbose)) // 로그 플러그인
+            NetworkLoggerPlugin(configuration: .init(logOptions: .verbose)) // 로그 플러그인
         ]
         
         // provider 초기화
@@ -22,17 +22,18 @@ public final class TastingNoteService: NetworkManager {
     //MARK: - DTO funcs
     /// 새 노트 작성을 위한 DTO 생성 함수
     /// 모든 항목이 required임
-    public func makePostNoteDTO(wineId : Int,
-                                color: String,
-                                tasteDate : String,
-                                sugarContent: Int,
-                                acidity : Int,
-                                tannin : Int,
-                                body : Int,
-                                alcohol : Int,
-                                nose : [String],
-                                rating : Double,
-                                review : String
+    public func makePostNoteDTO(
+        wineId : Int,
+        color: String,
+        tasteDate : String,
+        sugarContent: Int,
+        acidity : Int,
+        tannin : Int,
+        body : Int,
+        alcohol : Int,
+        nose : [String],
+        rating : Double,
+        review : String
     ) -> TastingNoteRequestDTO {
         return TastingNoteRequestDTO(
             wineId: wineId,
@@ -52,47 +53,78 @@ public final class TastingNoteService: NetworkManager {
     
     /// 노트 수정을 위한 DTO 생성 함수
     /// 모든 항목이 required임
-    public func makeUpdateNoteDTO(noteId : Int,
-                                  body : TastingNoteUpdateRequestDTO) -> TastingNotePatchRequestDTO {
-        return TastingNotePatchRequestDTO(noteId: noteId, body: body)
+    public func makeUpdateNoteDTO(
+        noteId : Int,
+        body : TastingNoteUpdateRequestDTO
+    ) -> TastingNotePatchRequestDTO {
+        return TastingNotePatchRequestDTO(
+            noteId: noteId,
+            body: body
+        )
     }
     
     /// 노트 수정DTO의 body을 위한 DTO 생성 함수
     /// 모든 항목이 optional임
-    public func makeUpdateNoteBodyDTO(color : String? = nil,
-                                      tastingDate : String? = nil,
-                                      sugarContent : Int? = nil,
-                                      acidity : Int? = nil,
-                                      tannin : Int? = nil,
-                                      body : Int? = nil,
-                                      alcohol : Int? = nil,
-                                      updateNoseList : [String]? = nil,
-                                      satisfaction : Double? = nil,
-                                      review : String? = nil
+    public func makeUpdateNoteBodyDTO(
+        color : String? = nil,
+        tastingDate : String? = nil,
+        sugarContent : Int? = nil,
+        acidity : Int? = nil,
+        tannin : Int? = nil,
+        body : Int? = nil,
+        alcohol : Int? = nil,
+        updateNoseList : [String]? = nil,
+        satisfaction : Double? = nil,
+        review : String? = nil
     ) -> TastingNoteUpdateRequestDTO {
-        return TastingNoteUpdateRequestDTO(color: color, tastingDate: tastingDate, sugarContent: sugarContent, acidity: acidity, tannin: tannin, body: body, alcohol: alcohol, updateNoseList: updateNoseList, rating: satisfaction, review: review)
+        return TastingNoteUpdateRequestDTO(
+            color: color,
+            tastingDate: tastingDate,
+            sugarContent: sugarContent,
+            acidity: acidity,
+            tannin: tannin,
+            body: body,
+            alcohol: alcohol,
+            updateNoseList: updateNoseList,
+            rating: satisfaction,
+            review: review
+        )
     }
     
     //MARK: - API funcs
     
     /// 새 노트 작성 API
     public func postNote(data: TastingNoteRequestDTO) async throws -> String {
-        try await requestAsync(target: .postNote(data: data), decodingType: String.self)
+        try await requestAsync(
+            target: .postNote(
+                data: data
+            ),
+            decodingType: String.self
+        )
     }
     
     /// 노트 수정 API
     public func patchNote(data: TastingNotePatchRequestDTO) async throws -> String {
-        try await requestAsync(target: .patchNote(data: data), decodingType: String.self)
+        try await requestAsync(
+            target: .patchNote(data: data),
+            decodingType: String.self
+        )
     }
     
     /// 노트 삭제 API
     public func deleteNote(noteId: Int) async throws -> String {
-        try await requestAsync(target: .deleteNote(noteId: noteId), decodingType: String.self)
+        try await requestAsync(
+            target: .deleteNote(noteId: noteId),
+            decodingType: String.self
+        )
     }
     
     /// 특정 노트 정보 조회 API
     public func fetchNote(noteId: Int) async throws ->TastingNoteResponsesDTO {
-        try await requestAsync(target: .getNote(noteId: noteId), decodingType: TastingNoteResponsesDTO.self)
+        try await requestAsync(
+            target: .getNote(noteId: noteId),
+            decodingType: TastingNoteResponsesDTO.self
+        )
     }
     
     /// 모든 노트 정보 조회 API -> 기본이 0
