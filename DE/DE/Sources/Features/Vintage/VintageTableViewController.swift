f575bf6ed10264855f6cd65888e8d7a6bbe3e11a
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2025-08-31 01:48:33 +0900

    #169/fix: push/pop 시점에 탭바, 내비게이션바 셋업

--- DE/DE/Sources/Features/Vintage/VintageTableViewController.swift
+++ DE/DE/Sources/Features/Vintage/VintageTableViewController.swift
@@ -33,13 +33,6 @@ public final class VintageTableViewController: UIViewController {
     public override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
-        self.tabBarController?.tabBar.isHidden = true
-    }
-    
-    public override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
-        self.tabBarController?.tabBar.isHidden = false
     }
     
     // MARK: - Setup
